56460d764e7e856d3d6469384140ed46c60ff566
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2021-10-25 17:45:00 +0530

    feat - added home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <div className="app">
       <h1>ShopEzy Store 🚀</h1>
+      <Home />
     </div>
   );
 }
