feffbbc2fbcae259eec07ca657a0a186fdfc72f5
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2021-10-27 16:05:21 +0530

    Feat- created HomeBody Component

--- src/Components/HomeBody/HomeBody.js
+++ src/Components/HomeBody/HomeBody.js
@@ -1,9 +1,53 @@
 import React from "react";
+import useCustomContext from "../../Hooks/UseCustomContext";
+import SingleProduct from "../../Components/SingleProduct/SingleProduct";
+import "./HomeBody.css";
+import FilterSidebar from "../FilterSidebar/FilterSidebar";
 
 const HomeBody = () => {
+  const { products, sortBy, byStock, byFastDelivery, byRating, searchQuery } =
+    useCustomContext();
+
+  const filterProducts = () => {
+    let filteredProducts = products;
+
+    if (sortBy) {
+      filteredProducts = filteredProducts.sort((a, b) =>
+        sortBy === "LowToHigh" ? a.price - b.price : b.price - a.price
+      );
+    }
+    if (!byStock) {
+      filteredProducts = filteredProducts.filter(
+        (product) => product.inStock > 0
+      );
+    }
+    if (byFastDelivery) {
+      filteredProducts = filteredProducts.filter(
+        (product) => product.fastDelivery
+      );
+    }
+    if (byRating) {
+      filteredProducts = filteredProducts.filter(
+        (product) => product.ratings >= byRating
+      );
+    }
+    if (searchQuery) {
+      filteredProducts = filteredProducts.filter((product) =>
+        product.name.toLowerCase().includes(searchQuery)
+      );
+    }
+
+    return filteredProducts;
+  };
   return (
-    <div>
-      <h1>HOME BODY</h1>
+    <div className="home__section">
+      <FilterSidebar />
+      <div className="product__container">
+        {products.length > 0 &&
+          filterProducts().map((product) => (
+            <SingleProduct product={product} key={product.id}></SingleProduct>
+          ))}
+      </div>
     </div>
   );
 };
