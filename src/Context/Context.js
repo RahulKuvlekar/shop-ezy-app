983d6c003c2dfceb2ff461349d2d31ca7f9216c5
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2021-10-28 02:42:41 +0530

    Feat- fetched products

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -1,7 +1,8 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useState, useEffect } from "react";
 import CartReducer from "./CartReducer";
 import faker from "faker";
 import FilterReducer from "./FilterReducer";
+import db from "../Config/Firebase-Init";
 
 export const Context = createContext({
   user: null,
@@ -18,19 +19,35 @@ export const Context = createContext({
 faker.seed(89);
 
 const ContextProvider = (props) => {
-  const products = [...Array(20)].map(() => ({
-    id: faker.datatype.uuid(),
-    name: faker.commerce.productName(),
-    price: Number(faker.commerce.price()),
-    image: faker.random.image(),
-    inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
-    fastDelivery: faker.datatype.boolean(),
-    ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
-  }));
+  const [products, setProducts] = useState([]);
+
+  // const products = [...Array(20)].map(() => ({
+  //   id: faker.datatype.uuid(),
+  //   name: faker.commerce.productName(),
+  //   price: Number(faker.commerce.price()),
+  //   image: faker.random.image(),
+  //   inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
+  //   fastDelivery: faker.datatype.boolean(),
+  //   ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
+  // }));
+
+  const fetchProducts = () => {
+    console.log("FetchProducts");
+    db.collection("shop").onSnapshot((snapshot) =>
+      setProducts(
+        snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }))
+      )
+    );
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   const [cart, dispatchCart] = useReducer(CartReducer, {
     cartList: [],
-    productList: products,
   });
 
   const [filter, dispatchFilter] = useReducer(FilterReducer, {
@@ -41,11 +58,11 @@ const ContextProvider = (props) => {
     searchQuery: "",
   });
 
-  //console.log("PRODUCTS ====> \n", products);
+  // console.log("PRODUCTS ====> \n", firebaseProducts);
 
   const DEFAULT_VALUE = {
     user: null,
-    products: cart.productList,
+    products: products,
     cart: cart.cartList,
     dispatchCart: dispatchCart,
     dispatchFilter: dispatchFilter,
